08e461a954e69367c839fdcadfa85aa7314bd2aa
Ria Kakarlapudi <ria.kakarlapudi@gmail.com>
2024-05-12 06:28:09 -0500

    initial commit

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -8,5 +8,13 @@ namespace Classes
 {
     internal class Car
     {
+
+        public string Make { get; set; } //Property
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+
+
     }
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Car myMake = new Car();
+            Car myModel = new Car();
+            Car myYear = new Car();
+
+            myMake.Make = "Toyota";
+            myModel.Model = "Corolla";
+            myYear.Year = 2015;
+
+            Console.WriteLine(myMake);
+            Console.WriteLine(myModel);
+            Console.WriteLine(myYear);
+        
         }
     }
 }
